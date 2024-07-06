d2ddbf1c9f4d029c16cd98676e43add23cd3e5a7
Nikhil <nikhilgyanchandani10@gmail.com>
2024-07-06 12:21:49 +0530

    updated gitignore

--- config.py
+++ config.py
@@ -11,7 +11,6 @@ DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
-print("KEY:", ALPHA_VANTAGE_API_KEY)
 # Ensure all required variables are loaded
 required_vars = [ALPHA_VANTAGE_API_KEY, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME]
 if not all(required_vars):
