0604f8eaf4465d1b8d154d522fd01fec71d006ac
Nikhil <nikhilgyanchandani10@gmail.com>
2024-07-08 01:01:51 +0530

    added readme and db setup

--- stock_market_db.sql
+++ stock_market_db.sql
@@ -1,3 +1,5 @@
+-- Create historical_stock_prices DB table
+-- This table will store historical stock prices data
 CREATE TABLE historical_stock_prices (
     id SERIAL PRIMARY KEY,
     "Date" DATE NOT NULL,
@@ -9,3 +11,15 @@ CREATE TABLE historical_stock_prices (
     "Volume" BIGINT NOT NULL,
     CONSTRAINT unique_date_company UNIQUE ("Date", "Company")
 );
+
+-- Create index on Company and Date with all price columns
+-- This index will improve the performance of queries that filter by company and date and include price columns
+CREATE INDEX idx_company_date_prices ON historical_stock_prices ("Company", "Date", "Open", "Close", "High", "Low");
+
+-- Create index on Company, Date, and Volume
+-- This index will improve the performance of queries that filter by company and date and involve the volume column
+CREATE INDEX idx_company_date_volume ON historical_stock_prices ("Company", "Date", "Volume");
+
+-- Create index on Company and Date
+-- This index will improve the performance of general queries that filter by company and date
+CREATE INDEX idx_company_date ON historical_stock_prices ("Company", "Date");
